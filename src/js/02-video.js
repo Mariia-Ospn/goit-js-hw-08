acde452bfe9c8aab58891eed1bee67f9ae22a690
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-07-13 18:58:32 +0200

    Task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const savePlayerTime = ({ seconds }) => {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+};
+
+const loadPlayerTime = () => {
+  return localStorage.getItem(LOCALSTORAGE_KEY) || 0;
+};
+
+player
+  .setCurrentTime(loadPlayerTime())
+  .then(function (seconds) {
+    console.log(`Set current time: ${seconds} seconds`);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          'The time was less than 0 or greater than the video’s duration'
+        );
+        break;
+
+      default:
+        console.log('Some other error occurred');
+        break;
+    }
+  });
+
+player.on('timeupdate', throttle(savePlayerTime, 1000));
